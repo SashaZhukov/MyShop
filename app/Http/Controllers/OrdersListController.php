6c34473253ac35ad14aa75e0d06f496e674c997f
SashaZhukov <sasha01zhukov@gmail.com>
2024-05-01 14:15:54 +0300

    UserOrders

---
+++ app/Http/Controllers/OrdersListController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Currency;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrdersListController extends Controller
+{
+    public function index(Request $request){
+        $currencyActive = Currency::find($request->session()->get('currencies'));
+        $currency = Currency::all()->count();
+
+        $orders = Order::where('user_id', auth()->user()->id)->where('status', '!=', 'received')->get();
+
+        return view('myOrder.orders', compact('currency', 'currencyActive', 'orders'));
+    }
+
+}
